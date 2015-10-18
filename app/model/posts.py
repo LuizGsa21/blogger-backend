25e070f10d4304280d7b4fe455c76e07efff11aa
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-17 22:30:05 -0500

    Add table columns for posts

--- app/model/posts.py
+++ app/model/posts.py
@@ -2,4 +2,18 @@ from app.extensions import db
 from datetime import datetime
 
 class Posts(db.Model):
-    pass
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    categoryId = db.Column(db.Integer(), db.ForeignKey('categories.id'), nullable=False)
+    authorId = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=False)
+
+    title = db.Column(db.String(255))
+    body = db.Column(db.String(20000))
+    likes = db.Column(db.Integer())
+    slug = db.Column(db.String(255))
+
+    dateCreated = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
+    lastModified = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
+
+    comments = db.relationship('Comment', backref='posts', lazy='dynamic')
