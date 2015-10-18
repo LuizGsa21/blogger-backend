25c838669a0629997ad1aa0f3c46b9551e115994
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-17 22:47:06 -0500

    Add table columns for users

--- app/model/users.py
+++ app/model/users.py
@@ -1,5 +1,34 @@
 from app.extensions import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Users(db.Model):
-    pass
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(255), unique=True)
+
+    oauthId = db.Column(db.String(50))
+    oauthProvider = db.Column(db.String(50))
+
+    firstName = db.Column(db.String(50))
+    lastName = db.Column(db.String(50))
+
+    pwdhash = db.Column(db.String(255))
+
+    avatar_path = db.Column(db.String(255), default='avatar.jpg')
+    dateJoined = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    isAdmin = db.Column(db.Boolean(), default=False)
+
+    posts = db.relationship('Posts', backref='author', lazy='dynamic')
+    comments = db.relationship('Comments', backref='user', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Users, self).__init__(**kwargs)
+        self.pwdhash = generate_password_hash(self.pwdhash)
+
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
