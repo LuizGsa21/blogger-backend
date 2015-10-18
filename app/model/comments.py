02fc9eb262b8cec1823a3ce57bbce9c0b4254898
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-17 22:35:56 -0500

    Add table columns for comments

--- app/model/comments.py
+++ app/model/comments.py
@@ -1,5 +1,17 @@
 from app.extensions import db
-
+from datetime import datetime
 
 class Comments(db.Model):
-    pass
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    parentId = db.Column(db.Integer(), db.ForeignKey('comments.id'), nullable=True)
+    userId = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    postId = db.Column(db.Integer(), db.ForeignKey('posts.id'))
+
+    title = db.Column(db.String(200))
+    body = db.Column(db.String(10000))
+    likes = db.Column(db.Integer())
+
+    lastModified = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
+    dateCreated = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
