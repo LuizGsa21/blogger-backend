2a93a5c599294798f9c6e346966b9b7649bd54aa
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-18 18:54:39 -0500

    Add resource for comments

---
+++ app/resources/comments.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify
+from app.model import Comments
+
+comments_bp = Blueprint('comments', __name__, url_prefix='/api/v1/comments')
+
+
+@comments_bp.route('', methods=['GET'])
+def get_comments():
+    return ''
+
+@comments_bp.route('/<int:id>', methods=['GET'])
+def get_comment_by_id(id):
+    return ''
+
+@comments_bp.route('', methods=['POST'])
+def post_comments():
+    return ''
+
+@comments_bp.route('', methods=['PUT'])
+def put_comments():
+    return ''
+
+@comments_bp.route('', methods=['DELETE'])
+def delete_comments():
+    return ''
