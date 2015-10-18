096808da49e807caf7e84427341deada78dce6d7
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-17 22:38:21 -0500

    Add table columns for categories

--- app/model/categories.py
+++ app/model/categories.py
@@ -2,4 +2,9 @@ from app.extensions import db
 
 
 class Categories(db.Model):
-    pass
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    
+    posts = db.relationship('Posts', backref='category', lazy='dynamic')
