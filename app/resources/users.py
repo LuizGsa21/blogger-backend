be8729a15394c961b2ba202901abc819949d4af8
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-18 19:23:22 -0500

    Add enpoint template for users resource

--- app/resources/users.py
+++ app/resources/users.py
@@ -1,14 +1,25 @@
 from flask import Blueprint, jsonify
 from app.model import Posts
 
-users_bp = Blueprint('api', __name__, url_prefix='/api/users')
+users_bp = Blueprint('users', __name__, url_prefix='/api/v1/users')
 
 
-@users_bp.route('/', methods=['GET'])
+@users_bp.route('', methods=['GET'])
 def get_users():
-    pass
+    return ''
 
-
-@users_bp.route('/users/<int:id>', methods=['GET'])
+@users_bp.route('/<int:id>', methods=['GET'])
 def get_user_by_id(id):
-    pass
+    return ''
+
+@users_bp.route('', methods=['POST'])
+def post_users():
+    return ''
+
+@users_bp.route('', methods=['PUT'])
+def put_users():
+    return ''
+
+@users_bp.route('', methods=['DELETE'])
+def delete_users():
+    return ''
