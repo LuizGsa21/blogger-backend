cfdf4270fb4da602252d67aa9ce1a4ce6baf1abf
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-20 00:25:40 -0500

    Add CaseInsensitiveWord comparator

---
+++ app/model/sqlalchemy_helpers.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.hybrid import Comparator
+from sqlalchemy.sql import func
+
+
+class CaseInsensitiveWord(Comparator):
+    """Hybrid value representing a lower case representation of a word."""
+
+    def __init__(self, word):
+        if isinstance(word, basestring):
+            self.word = word.lower()
+        elif isinstance(word, CaseInsensitiveWord):
+            self.word = word.word
+        else:
+            self.word = func.lower(word)
+
+    def operate(self, op, other):
+        if not isinstance(other, CaseInsensitiveWord):
+            other = CaseInsensitiveWord(other)
+        return op(self.word, other.word)
+
+    def __clause_element__(self):
+        return self.word
+
+    def __str__(self):
+        return self.word
