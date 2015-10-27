c3518b831b9c49bb605ae52301027785663e0992
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-27 17:09:09 -0500

    Feat: implement `put_article_by_id()`

--- app/resources/articles.py
+++ app/resources/articles.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Articles
-from app.schemas import article_resource_serializer
+from app.schemas import article_resource_serializer, article_serializer
 from app.extensions import db
+import json
 
 articles_bp = Blueprint('articles', __name__, url_prefix='/api/v1/articles')
 
@@ -25,9 +26,14 @@ def post_articles():
     return ''
 
 
-@articles_bp.route('', methods=['PUT'])
-def put_articles():
-    return ''
+@articles_bp.route('/<int:id>', methods=['PUT'])
+def put_article_by_id(id):
+    response = json.loads(request.data)
+    data, errors, = article_serializer.dump(response['data']['attributes'])
+    Articles.query.filter_by(id=id).update(data)
+    db.session.commit()
+    data, errors = article_resource_serializer.dump(Articles.query.get(id))
+    return jsonify(data=data)
 
 
 @articles_bp.route('/<int:id>', methods=['DELETE'])
