3aab82a43208cba900f04710e11fb271278783dc
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-10-27 16:47:47 -0500

    Feat: implement `get_article_by_id()`

--- app/resources/articles.py
+++ app/resources/articles.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from app.models import Articles
 from app.schemas import article_resource_serializer
+
 articles_bp = Blueprint('articles', __name__, url_prefix='/api/v1/articles')
 
 
@@ -10,17 +11,23 @@ def get_articles():
     data, errors = article_resource_serializer.dump(articles, many=True)
     return jsonify(data=data)
 
+
 @articles_bp.route('/<int:id>', methods=['GET'])
-def get_post_by_id(id):
-    return ''
+def get_article_by_id(id):
+    article = Articles.query.get(id)
+    data, errors = article_resource_serializer.dump(article)
+    return jsonify(data=data)
+
 
 @articles_bp.route('', methods=['POST'])
 def post_articles():
     return ''
 
+
 @articles_bp.route('', methods=['PUT'])
 def put_articles():
     return ''
+
 
 @articles_bp.route('', methods=['DELETE'])
 def delete_articles():
