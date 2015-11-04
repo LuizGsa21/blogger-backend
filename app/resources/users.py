74a60bcb909870a68497dda715f369da8b9e6f6b
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-11-04 10:35:19 -0600

    feat: update user endpoints to use the new schemas

--- app/resources/users.py
+++ app/resources/users.py
@@ -1,10 +1,19 @@
+import inspect
 import pprint
 import json
 from flask import Blueprint, jsonify, request
 from flask_login import current_user
 from app.models import Articles, Users
-from app.schemas import user_post_serializer, user_serializer, user_resource_serializer
 from app.extensions import db
+from app.utils.decorators import admin_required, login_required
+from app.utils.errors import Error, AdminRequiredError
+from app.schemas import (
+    create_user_serializer,
+    create_user_admin_serializer,
+    edit_user_profile_serializer,
+    edit_user_admin_serializer,
+    view_user_serializer
+)
 
 users_bp = Blueprint('users', __name__, url_prefix='/api/v1/users')
 
@@ -12,14 +21,15 @@ users_bp = Blueprint('users', __name__, url_prefix='/api/v1/users')
 @users_bp.route('', methods=['GET'])
 def get_users():
     users = Users.query.all()
-    data, errors = user_resource_serializer.dump(users, many=True)
+    data, errors = view_user_serializer.dump(users, many=True)
     return jsonify(data=data)
 
 
 @users_bp.route('/<int:id>', methods=['GET'])
 def get_user_by_id(id):
     users = Users.query.get(id)
-    data, errors = user_resource_serializer.dump(users)
+    data, errors = view_user_serializer.dump(users)
+    data['relationships'] = users.get_relationships()
     return jsonify(data=data)
 
 
@@ -32,39 +42,55 @@ def post_users():
         }])
         response.status_code = 400
         return response
-
-    data, errors = user_post_serializer.loads(data)
-    if errors:
-        response = jsonify(errors=errors)
-        # If there is more than one error, use a 400 status code
-        if len(errors) > 1:
-            response.status_code = 400
-        else:
-            response.status_code = errors.itervalues().next()[0]['status']
-        return response
-    user = Users(**data)
+    if current_user.is_admin:
+        data, errors = create_user_admin_serializer.loads(data)
+    else:
+        data, errors = create_user_serializer.loads(data)
+    user = Users(**data['attributes'])
     db.session.add(user)
     db.session.commit()
-    response = jsonify(data=user_resource_serializer.dump(user).data)
+    data, errors = edit_user_profile_serializer.dump(user)
+    response = jsonify(data=data)
     response.status_code = 201
     return response
 
 
 @users_bp.route('/<int:id>', methods=['PUT'])
+@admin_required
 def put_user_by_id(id):
-    response = json.loads(request.data)
-    data, errors, = user_serializer.dump(response['data']['attributes'])
-    Users.query.filter_by(id=id).update(data)
+    # Note: only admins can access this endpoint.
+    data, _ = edit_user_admin_serializer.loads(request.data)
+    Users.query.filter_by(id=id).update(data['attributes'])
     db.session.commit()
-    return jsonify(data=user_resource_serializer.dump(Users.query.get(id)).data)
+    response = jsonify()
+    response.status_code = 204
+    return response
+
+@users_bp.route('/<int:id>', methods=['PATCH'])
+@login_required
+def patch_user_by_id(id):
+    if current_user.is_admin:
+        data, errors, = edit_user_admin_serializer.loads(request.data)
+    elif current_user.id != id:
+        raise AdminRequiredError()
+    else:
+        data, errors, = edit_user_profile_serializer.loads(request.data)
+    Users.query.filter_by(id=id).update(data['attributes'])
+    db.session.commit()
+    response = jsonify()
+    response.status_code = 204
+    return response
 
 
 @users_bp.route('/<int:id>', methods=['DELETE'])
+@admin_required
 def delete_user_by_id(id):
-    if not current_user.is_admin or current_user.id == id:
-        response = jsonify(errors=[{'detail': 'You are not authorized to perform this operation.'}])
+    if current_user.id == id:
+        response = jsonify(errors=[{'detail': "You can't delete yourself."}])
         response.status_code = 403
         return response
     Users.query.filter_by(id=id).delete()
     db.session.commit()
-    return jsonify(data=None)
+    response = jsonify()
+    response.status_code = 204
+    return response
