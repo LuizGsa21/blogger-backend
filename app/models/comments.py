2a3db7bf9c52d4f50593d32edae4960668856f94
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-11-05 15:44:52 -0600

    feat: implement column filter for Comments

--- app/models/comments.py
+++ app/models/comments.py
@@ -2,6 +2,8 @@ from app.extensions import db
 from flask import url_for
 from datetime import datetime
 import meta
+from app.utils import Role, Method
+
 
 class Comments(db.Model, meta.ResourceMixin):
     __tablename__ = 'comments'
@@ -24,3 +26,26 @@ class Comments(db.Model, meta.ResourceMixin):
     def url(self):
         return url_for('comments.get_comment_by_id', id=self.id, _external=True)
 
+    @classmethod
+    def get_guest_columns(cls, method):
+        if method == Method.CREATE:
+            return None
+        if method == Method.READ:
+            return 'title', 'body', 'lastModified', 'dateCreated'
+        if method == Method.UPDATE:
+            return None
+        if method == Method.DELETE:
+            return None
+        raise RuntimeError('Unknown METHOD: {}'.format(method))
+
+    @classmethod
+    def get_user_columns(cls, method):
+        if method == Method.CREATE:
+            return 'title', 'body', 'parentId', 'userId'
+        if method == Method.READ:
+            return 'id', 'title', 'body', 'parentId', 'userId', 'lastModified', 'dateCreated'
+        if method == Method.UPDATE:
+            return 'title', 'body'
+        if method == Method.DELETE:
+            return None
+        raise RuntimeError('Unknown METHOD: {}'.format(method))
