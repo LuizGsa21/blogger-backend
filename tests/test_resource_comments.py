c71f9a3ef7f433c4530e35543fa584ee877c47ce
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-11-05 14:57:12 -0600

    test: add test case for get_comment_by_id

--- tests/test_resource_comments.py
+++ tests/test_resource_comments.py
@@ -14,3 +14,16 @@ class ResourceCommentsTestCase(TestCase):
         self.assert_200_ok(response)
         self.assert_resource_count(response, count)
 
+    def test_get_comment_by_id(self):
+        count = Comments.query.count()
+        assert count > 2  # verify fixture
+
+        response = self.client.get('/api/v1/comments/1', content_type='application/json')
+        self.assert_200_ok(response)
+        self.assert_resource_response(response,
+                                      type_='comments',
+                                      id_=1,
+                                      attributes_keys=['title', 'body', 'lastModified', 'dateCreated'],
+                                      attributes_exclude=['parentId', 'userId', 'articleId'],
+                                      links='self')
+
