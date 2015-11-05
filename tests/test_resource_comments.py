3759b3ee41e6abf749f863ae1fe3efcf1efb1429
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-11-05 16:28:15 -0600

    test: add test case for post_comments

--- tests/test_resource_comments.py
+++ tests/test_resource_comments.py
@@ -41,21 +41,73 @@ class ResourceCommentsTestCase(TestCase):
         resource_json = json.dumps(resource)
         comment = Comments.query.get(1)
 
-        # attempt to update an comment using an anonymous user. (This should fail)
+        # attempt to update a comment using a anonymous user. (This should fail)
         response = self.client.put('/api/v1/comments/1', data=resource_json, content_type='application/json')
         self.assert_401_unauthorized(response)
         self.assert_resource(resource, negate_attributes=True)
 
-        # attempt to update an comment using an registered who doesn't have ownership. (This should fail)
+        # attempt to update a comment using a registered who doesn't have ownership. (This should fail)
         self.login_with_id(comment.userId + 1)
         response = self.client.put('/api/v1/comments/1', data=resource_json, content_type='application/json')
         self.assert_403_forbidden(response)
         self.assert_resource(resource, negate_attributes=True)
         self.logout()
 
-        # attempt to update an comment using an registered who has ownership. (This should NOT fail)
+        # attempt to update a comment using a registered who has ownership. (This should NOT fail)
         self.login_with_id(comment.userId)
         response = self.client.put('/api/v1/comments/1', data=resource_json, content_type='application/json')
         self.assert_204_no_content(response)
+        self.assert_resource(resource)
+
+    def _get_post_resource(self):
+        return {
+            'data': {
+                'type': 'comments',
+                'attributes': {
+                    'title': 'New Title',
+                    'body': 'New Body'
+                },
+                'relationships': {
+                    'user': {
+                        'type': 'users',
+                        'id': '1'
+                    },
+                    'comment': {
+                        'type': 'comment',
+                        'id': None
+                    },
+                    'article': {
+                        'type': 'articles',
+                        'id': '1'
+                    }
+                }
+            }
+        }
+
+    def test_post_comments(self):
+        resource = self._get_post_resource()
+        resource_json = json.dumps(resource)
+        # attempt to create a comment using an anonymous user. (This should fail)
+        response = self.client.post('/api/v1/comments', data=resource_json, content_type='application/json')
+        self.assert_401_unauthorized(response)
+
+        # make sure a user can't impersonate another user
+        self.login_with_id(1)
+        resource = self._get_post_resource()
+        resource['data']['relationships']['user']['id'] = '2'
+        resource_json = json.dumps(resource)
+        response = self.client.post('/api/v1/comments', data=resource_json, content_type='application/json')
+        self.assert_403_forbidden(response)
+
+        # create a comment using a registered user. (This should NOT fail)
+        resource = self._get_post_resource()
+        resource_json = json.dumps(resource)
+        response = self.client.post('/api/v1/comments', data=resource_json, content_type='application/json')
+        self.assert_201_created(response)
+        self.assert_resource_response(response,
+                                      type_='comments',
+                                      attributes_keys=['title', 'body'],
+                                      links='self')
+        resource['data']['id'] = response.get_json()['data']['id']
         self.assert_resource(resource)
 
