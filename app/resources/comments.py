cd92d3f5f962f43a6d62d60c0e9509ccbd6a7b74
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-11-05 15:59:13 -0600

    feat: implement put_comment_by_id

--- app/resources/comments.py
+++ app/resources/comments.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, request
-from app.utils import jsonify, PageNotFoundError, login_required
+from app.utils import jsonify, PageNotFoundError, PermissionDeniedError, login_required
 from app.models import Comments
-from app.schemas import read_comment_serializer, create_comment_serializer
+from app.extensions import db
+from app.schemas import (
+    read_comment_serializer,
+    create_comment_serializer,
+    update_comment_serializer
+)
+from flask_login import current_user
+
 comments_bp = Blueprint('comments', __name__, url_prefix='/api/v1/comments')
 
 
@@ -26,10 +33,21 @@ def post_comments():
 
     return ''
 
-@comments_bp.route('', methods=['PUT'])
+@comments_bp.route('/<int:id>', methods=['PUT'])
 @login_required
-def put_comments():
-    return ''
+def put_comment_by_id(id):
+    comment = Comments.query.filter_by(id=id).first()
+    if not comment:
+        raise PageNotFoundError()
+    if not current_user.is_admin and comment.userId != current_user.id:
+        raise PermissionDeniedError('edit', 'comment')
+
+    data, _ = update_comment_serializer.loads(request.data)
+    Comments.query.filter_by(id=id).update(data['attributes'])
+    db.session.commit()
+    response = jsonify()
+    response.status_code = 204
+    return response
 
 @comments_bp.route('', methods=['DELETE'])
 @login_required
