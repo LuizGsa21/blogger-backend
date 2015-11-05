ac18bcae0dc409c629fe0b39809b84570f889767
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-11-05 14:40:12 -0600

    fix typo

--- tests/test_resource_articles.py
+++ tests/test_resource_articles.py
@@ -43,14 +43,14 @@ class ResourceArticlesTestCase(TestCase):
         self.assert_401_unauthorized(response)
         self.assert_resource(resource, negate_attributes=True)
 
-        # attempt to update an article using an registered who doesn't have ownership. (This should fail)
+        # attempt to update an article using a registered user without ownership. (This should fail)
         self.login('bob1', 'mypassword')
         response = self.client.put('/api/v1/articles/1', data=resource_json, content_type='application/json')
         self.assert_403_forbidden(response)
         self.assert_resource(resource, negate_attributes=True)
         self.logout()
 
-        # attempt to update an article using an registered who has ownership. (This should NOT fail)
+        # attempt to update an article using a registered user with ownership. (This should NOT fail)
         self.login_with_id(Articles.query.get(1).authorId)
         response = self.client.put('/api/v1/articles/1', data=resource_json, content_type='application/json')
         self.assert_204_no_content(response)
