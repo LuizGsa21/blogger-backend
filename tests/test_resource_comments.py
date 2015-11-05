0ca8d3df02913aab6aef3dc68ac0fe5ad00f445a
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-11-05 15:58:20 -0600

    test: add test case for put_comment_by_id

--- tests/test_resource_comments.py
+++ tests/test_resource_comments.py
@@ -27,3 +27,35 @@ class ResourceCommentsTestCase(TestCase):
                                       attributes_exclude=['parentId', 'userId', 'articleId'],
                                       links='self')
 
+    def test_put_comment_by_id(self):
+        resource = {
+            'data': {
+                'type': 'comments',
+                'id': 1,
+                'attributes': {
+                    'title': 'New Title',
+                    'body': 'New Body'
+                }
+            }
+        }
+        resource_json = json.dumps(resource)
+        comment = Comments.query.get(1)
+
+        # attempt to update an comment using an anonymous user. (This should fail)
+        response = self.client.put('/api/v1/comments/1', data=resource_json, content_type='application/json')
+        self.assert_401_unauthorized(response)
+        self.assert_resource(resource, negate_attributes=True)
+
+        # attempt to update an comment using an registered who doesn't have ownership. (This should fail)
+        self.login_with_id(comment.userId + 1)
+        response = self.client.put('/api/v1/comments/1', data=resource_json, content_type='application/json')
+        self.assert_403_forbidden(response)
+        self.assert_resource(resource, negate_attributes=True)
+        self.logout()
+
+        # attempt to update an comment using an registered who has ownership. (This should NOT fail)
+        self.login_with_id(comment.userId)
+        response = self.client.put('/api/v1/comments/1', data=resource_json, content_type='application/json')
+        self.assert_204_no_content(response)
+        self.assert_resource(resource)
+
